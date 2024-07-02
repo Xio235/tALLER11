d6f5377af57c7556d266f4c2d47da5a26e16a466
Antonio <159509348+Xio235@users.noreply.github.com>
2024-07-02 16:38:01 -0500

    Update funciones.h

--- Nueva-carpeta/funciones.h
+++ Nueva-carpeta/funciones.h
@@ -1,11 +1,8 @@
-void guardarHuespedes(char huespedes[][2][40], const char *filename);
-void guardarReservas(int reservas[][4], const char *filename);
-void cargarHuespedes(char huespedes[][2][40], const char *filename);
-void cargarReservas(int reservas[][4], const char *filename);
-
 void buscarPorTamano(int *numeroHabitacion, char cuartos[][3][40], double costos[]);
 void buscarHotel(int *numeroHabitacion, char cuartos[][3][40], double costos[]);      
 void realizarReserva(int numeroHabitacion, char cuartos[][3][40], char huespedes[][2][40], int reservaciones[][4]);
 void buscarReservaPorCedula(int *numeroReserva, char huespedes[][2][40], int reservaciones[][4]);
 void imprimirReserva(int numeroReserva, int reservaciones[][4], char cuartos[][3][40], double costos[], char huespedes[][2][40]);
-void pagarReserva(int numeroReserva, int reservaciones[][4], char cuartos[][3][40], double costos[]);
+void pagarReserva(int numeroReserva, int reservaciones[][4], char cuartos[][3][40], double costos[], char huespedes[][2][40]);
+void guardarDatos(char huespedes[][2][40], int reservaciones[][4]);
+void cargarDatos(char huespedes[][2][40], int reservaciones[][4]);
